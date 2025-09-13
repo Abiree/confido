e50e189051c645546fff43dd3d687ed20ab4991b
AbireGh <abire.ghallabi@dz-advisory.com>
2025-09-14 00:01:15 +0100

    feat: add import/formatting plugins for Angular and Spotless for Spring Boot

--- apps/webapp/src/app/app.spec.ts
+++ apps/webapp/src/app/app.spec.ts
@@ -1,7 +1,8 @@
 import { TestBed } from '@angular/core/testing';
+import { beforeEach, describe, expect, it } from 'vitest';
+
 import { App } from './app';
 import { NxWelcome } from './nx-welcome';
-import { describe, it, expect, beforeEach } from 'vitest';
 
 describe('App', () => {
   beforeEach(async () => {
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,9 @@
 import nx from '@nx/eslint-plugin';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import pluginPrettier from 'eslint-plugin-prettier';
+import unusedImports from 'eslint-plugin-unused-imports';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import simpleImport from 'eslint-plugin-import';
 
 export default [
   ...nx.configs['flat/base'],
@@ -9,7 +14,16 @@ export default [
   },
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    plugins: {
+      prettier: pluginPrettier, // Add Prettier plugin
+      'unused-imports': unusedImports, // Add unused-imports plugin
+      'simple-import-sort': simpleImportSort,
+      import: simpleImport,
+    },
     rules: {
+      // Integrate Prettier rules
+      ...eslintConfigPrettier.rules, // Turns off ESLint rules that conflict with Prettier
+      'prettier/prettier': 'error', // Reports Prettier formatting issues as ESLint errors
       '@nx/enforce-module-boundaries': [
         'error',
         {
@@ -23,6 +37,21 @@ export default [
           ],
         },
       ],
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'import/first': 'error',
+      'import/newline-after-import': 'error',
+      'import/no-duplicates': 'error',
     },
   },
   {
